fb5aa068b25369be8e99cd8d6bb4ebc52d87f06a
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-11-05 11:46:04 +0530

    feat: update header to include descriptive title for CI/CD project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Sajith R Sampath</h1>
+      <h1>Sajith R Sampath's First CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
